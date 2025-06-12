ffc86727111b5f30365e367c5bd06eeb4900efb8
Jaskaran Singh <89383365+Jaskaranpro@users.noreply.github.com>
2025-06-12 18:06:20 +0530

    updated React-Vertical-Timeline Component

--- src/components/educationSection/Education.jsx
+++ src/components/educationSection/Education.jsx
@@ -1,7 +1,7 @@
-//import { React } from "react";
 import { VerticalTimeline, VerticalTimelineElement } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { IoMdSchool } from "react-icons/io";
+import { useEffect, useState } from "react";
 import "./Education.css"; 
 
 const educationData = [
@@ -21,7 +21,7 @@ const educationData = [
     duration: "2018 - 2019",
     grade: "70%",
   },
-    {
+  {
     degree: "High School 10th",
     stream: "PCM",
     institution: "SDA Inter College, Roorkee, Uttarakhand",
@@ -32,9 +32,24 @@ const educationData = [
 ];
 
 const Education = () => {
+  const [iconSize, setIconSize] = useState(56); // default size
+
+  useEffect(() => {
+    const updateSize = () => {
+      const width = window.innerWidth;
+      if (width < 480) setIconSize(32);
+      else if (width < 768) setIconSize(44);
+      else setIconSize(56);
+    };
+
+    updateSize();
+    window.addEventListener("resize", updateSize);
+    return () => window.removeEventListener("resize", updateSize);
+  }, []);
+
   return (
     <section className="education-section">
-      <h2 className="education-heading"> Education</h2>
+      <h2 className="education-heading">Education</h2>
       <VerticalTimeline lineColor="#fff">
         {educationData.map((edu, index) => (
           <VerticalTimelineElement
@@ -42,16 +57,16 @@ const Education = () => {
             className="vertical-timeline-element--education"
             date={edu.duration}
             iconStyle={{ background: "rgb(251 151 24 / var(--tw-bg-opacity, 1))", color: "#fff" }}
-            icon={<IoMdSchool alt="School-Icon" className="icon-responsive" style={{ width: "56px", height: "175px", marginLeft: "-29px", marginTop: "-90px" , borderRadius: "50%" }} />}
+            icon={<IoMdSchool className="responsive-icon" size={iconSize} />}
           >
             <div className="timeline-elements">
-            <h3 className="vertical-timeline-element-title"><b>{edu.degree}</b></h3>
-            <h4 className="vertical-timeline-element-streamtitle">{edu.stream}</h4>
-            <h4 className="vertical-timeline-element-subtitle">{edu.institution}</h4>
-            {edu.university && <p>{edu.university}</p>}
-            {edu.board && <p>{edu.board}</p>}
-              <p style={{display: "flex"}}><strong>Grade: </strong> {edu.grade}</p>
-              </div>
+              <h3 className="vertical-timeline-element-title"><b>{edu.degree}</b></h3>
+              <h4 className="vertical-timeline-element-streamtitle">{edu.stream}</h4>
+              <h4 className="vertical-timeline-element-subtitle">{edu.institution}</h4>
+              {edu.university && <p>{edu.university}</p>}
+              {edu.board && <p>{edu.board}</p>}
+              <p><strong>Grade: </strong> {edu.grade}</p>
+            </div>
           </VerticalTimelineElement>
         ))}
       </VerticalTimeline>
